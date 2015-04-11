e7b1cfcf8bd7b852dc2f63e5f43900d2d29c9cab
Takashi Ando <dodo5522@gmail.com>
2015-04-12 08:45:55 +0900

    Add function to update all data on xively.

--- python/main.py
+++ python/main.py
@@ -3,22 +3,62 @@
 
 """ This is main program to test another modules. """
 
+import argparse
+import datetime
+import xively
+from driver import data
+
 __author__ = "Takashi Ando"
+__version__ = "0.0.2"
 __copyright__ = "Copyright 2015, My own project"
-__credits__ = ["My wife"]
 __license__ = "GPL"
-__version__ = "0.0.1"
-__maintainer__ = "Takashi Ando"
-__email__ = "noreply@temp.com"
-__status__ = "Production"
 
-if __name__ == "__main__":
-    import sys
-    from driver import data
 
-    host = sys.argv[1]
+def init_args():
+    arg = argparse.ArgumentParser(
+        description="main program to test TS-MPPT-60 monitor modules")
+    arg.add_argument(
+        "-n", "--host-name",
+        type=str,
+        help="TS-MPPT-60 host address"
+    )
+    arg.add_argument(
+        "-a", "--api-key",
+        type=str,
+        help="Xively API key string"
+    )
+    arg.add_argument(
+        "-f", "--feed-key",
+        type=int,
+        help="Xively feed key"
+    )
 
-    live = data.LiveData(host)
+    return arg.parse_args()
+
+
+def main(args):
+    api = xively.XivelyAPIClient(args.api_key)
+    feed = api.feeds.get(args.feed_key)
+
+    now = datetime.datetime.utcnow()
+    datastreams = []
+
+    live = data.LiveData(args.host_name)
+
     for group in live._data_objects:
         for data_in_group in live._data_objects[group].get_all():
-            print(data_in_group)
+            print(group + " : " + ", ".join(data_in_group))
+            datastreams.append(
+                xively.Datastream(
+                    id="".join(data_in_group[0].split()),
+                    current_value=float(data_in_group[1]),
+                    at=now
+                )
+            )
+
+    feed.datastreams = datastreams
+    feed.update()
+
+
+if __name__ == "__main__":
+    main(init_args())
