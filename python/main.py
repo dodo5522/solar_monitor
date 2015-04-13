acd220e35bfa67078686c568cb5e04ecf874f9f9
Takashi Ando <dodo5522@gmail.com>
2015-04-14 00:30:49 +0900

    Support timer threading.

--- python/main.py
+++ python/main.py
@@ -3,15 +3,39 @@
 
 """ This is main program to test another modules. """
 
+import time
+import threading
 import argparse
 import datetime
 import xively
-from driver import data
+import driver.data
 
 __author__ = "Takashi Ando"
 __version__ = "0.0.2"
 __copyright__ = "Copyright 2015, My own project"
 __license__ = "GPL"
+
+
+class RecursiveTimer(object):
+    """ Class of timer for recursively running function. """
+    def __init__(self, func, intarval_sec=300, is_resursive=True):
+        self._func = func
+        self._interval = intarval_sec
+        self._is_recursive = is_resursive
+        self._thread = threading.Timer(self._interval, self._tick)
+
+    def _tick(self):
+        if self._is_recursive:
+            self._thread = threading.Timer(self._interval, self._tick)
+            self._thread.start()
+
+        self._func()
+
+    def start(self):
+        self._thread.start()
+
+    def cancel(self):
+        self._thread.cancel()
 
 
 def init_args():
@@ -20,6 +44,7 @@ def init_args():
     arg.add_argument(
         "-n", "--host-name",
         type=str,
+        default="192.168.1.20",
         help="TS-MPPT-60 host address"
     )
     arg.add_argument(
@@ -32,6 +57,12 @@ def init_args():
         type=int,
         help="Xively feed key"
     )
+    arg.add_argument(
+        "-i", "--interval",
+        type=int,
+        default=300,
+        help="Xively update interval with sec"
+    )
 
     return arg.parse_args()
 
@@ -40,24 +71,35 @@ def main(args):
     api = xively.XivelyAPIClient(args.api_key)
     feed = api.feeds.get(args.feed_key)
 
-    now = datetime.datetime.utcnow()
-    datastreams = []
+    def update():
+        print("update called")
+        now = datetime.datetime.utcnow()
+        datastreams = []
 
-    live = data.LiveData(args.host_name)
+        live = driver.data.LiveData(args.host_name)
 
-    for group in live._data_objects:
-        for data_in_group in live._data_objects[group].get_all():
-            print(group + " : " + ", ".join(data_in_group))
-            datastreams.append(
-                xively.Datastream(
-                    id="".join(data_in_group[0].split()),
-                    current_value=float(data_in_group[1]),
-                    at=now
+        for group in live._data_objects:
+            for data_in_group in live._data_objects[group].get_all():
+                print(group + " : " + ", ".join(data_in_group))
+                datastreams.append(
+                    xively.Datastream(
+                        id="".join(data_in_group[0].split()),
+                        current_value=float(data_in_group[1]),
+                        at=now
+                    )
                 )
-            )
 
-    feed.datastreams = datastreams
-    feed.update()
+        feed.datastreams = datastreams
+        feed.update()
+
+    timer = RecursiveTimer(update, args.interval)
+
+    try:
+        timer.start()
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        timer.cancel()
 
 
 if __name__ == "__main__":
