61e97b649c4cfd49f5a92bc3b2b64d3ec5930972
Takashi Ando <dodo5522@gmail.com>
2015-04-21 09:50:24 +0900

    Add Main class.

--- python/tsmppt60_monitor.py
+++ python/tsmppt60_monitor.py
@@ -39,65 +39,105 @@ class RecursiveTimer(object):
         self._thread.cancel()
 
 
-def init_args():
-    arg = argparse.ArgumentParser(
-        description="main program to test TS-MPPT-60 monitor modules")
-    arg.add_argument(
-        "-n", "--host-name",
-        type=str,
-        default="192.168.1.20",
-        help="TS-MPPT-60 host address"
-    )
-    arg.add_argument(
-        "-a", "--api-key",
-        type=str,
-        help="Xively API key string"
-    )
-    arg.add_argument(
-        "-f", "--feed-key",
-        type=int,
-        help="Xively feed key"
-    )
-    arg.add_argument(
-        "-i", "--interval",
-        type=int,
-        default=300,
-        help="Xively update interval with sec"
-    )
-    arg.add_argument(
-        "--debug",
-        action='store_true',
-        default=False,
-        help="Enable debug mode"
-    )
+class Main(object):
+    """ main routine class """
 
-    return arg.parse_args()
+    def __init__(self):
+        arg = argparse.ArgumentParser(
+            description="main program to test TS-MPPT-60 monitor modules")
+        arg.add_argument(
+            "-n", "--host-name",
+            type=str,
+            default="192.168.1.20",
+            help="TS-MPPT-60 host address"
+        )
+        arg.add_argument(
+            "-a", "--api-key",
+            type=str,
+            help="Xively API key string"
+        )
+        arg.add_argument(
+            "-f", "--feed-key",
+            type=int,
+            help="Xively feed key"
+        )
+        arg.add_argument(
+            "-i", "--interval",
+            type=int,
+            default=300,
+            help="Xively update interval with sec"
+        )
+        arg.add_argument(
+            "--just-get-status",
+            action='store_true',
+            default=False,
+            help="Just get status of charge controller"
+        )
+        arg.add_argument(
+            "--debug",
+            action='store_true',
+            default=False,
+            help="Enable debug mode"
+        )
 
+        self.args = arg.parse_args()
+        self.logger = logging.getLogger("main")
 
-def main(args):
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(
-        "%(asctime)s %(name)s %(levelname)s: %(message)s",
-        "%Y/%m/%d %p %l:%M:%S"))
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)s: %(message)s",
+            "%Y/%m/%d %p %l:%M:%S"))
 
-    logger = logging.getLogger("main")
-    logger.addHandler(handler)
+        self.logger.addHandler(handler)
 
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
+        if self.args.debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
 
-    api = xively.XivelyAPIClient(args.api_key)
-    feed = api.feeds.get(args.feed_key)
+    def __call__(self):
+        if self.args.just_get_status:
+            for data in self._get_data_streams(self.args.host_name):
+                self.logger.info("id:{0}, value:{1}".format(
+                    data._data["id"], data._data["current_value"]))
+        else:
+            timer = RecursiveTimer(self._update_xively, self.args.interval)
 
-    def update():
+            try:
+                timer.start()
+                while True:
+                    time.sleep(10)
+            except KeyboardInterrupt:
+                timer.cancel()
+
+    def _update_xively(self):
+        """ Update xively feed with data got with _get_data_streams().
+        """
+        api = xively.XivelyAPIClient(self.args.api_key)
+        feed = api.feeds.get(self.args.feed_key)
+
+        feed.datastreams = self._get_data_streams(self.args.host_name)
+        feed.update()
+
+    def _get_data_streams(self, host_name):
+        """ Get status data from charge controller and convert them to data
+            streams list for xively.
+
+        Keyword arguments:
+            host_name: ip address like 192.168.1.20 or
+                       host name can be resolved by DNS
+
+        Returns: xively.Datastream list
+        """
         now = datetime.datetime.utcnow()
         datastreams = []
 
-        live = driver.livedata.LiveData(args.host_name)
+        live = driver.livedata.LiveData(host_name)
 
         for group in live:
             for status_all in live[group].get_all_status():
-                logger.debug(group + ": " + ", ".join(status_all))
+                self.logger.debug(group + ": " + ", ".join(status_all))
+
                 datastreams.append(
                     xively.Datastream(
                         id="".join(status_all[0].split()),
@@ -106,18 +146,9 @@ def main(args):
                     )
                 )
 
-        feed.datastreams = datastreams
-        feed.update()
-
-    timer = RecursiveTimer(update, args.interval)
-
-    try:
-        timer.start()
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        timer.cancel()
+        return datastreams
 
 
 if __name__ == "__main__":
-    main(init_args())
+    main = Main()
+    main()
