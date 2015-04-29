f32b569fe6ce422e8c9dd45c84805924ca1116df
Takashi Ando <dodo5522@gmail.com>
2015-04-29 11:27:48 +0900

    Fix cancel implementation.

--- python/timer.py
+++ python/timer.py
@@ -13,54 +13,54 @@ import threading
 
 class RecursiveTimer(object):
     """ Class of timer for recursively running function. """
-    def __init__(self, func, interval_sec=300, is_resursive=True):
-        """ Recursive timer if is_recursive is True.
+    def __init__(self, target, interval=300):
+        """ Recursive timer
 
         Arguments:
-            func: callable object to run by timer event.
-            interval_sec: interval time as second
-            is_recursive: func is called recursively if true
+            target: callable object to run by timer event.
+            interval: interval time as second
         """
-        self._func = func
-        self._thread_tick = threading.Thread(
-            target=self._tick, args=(interval_sec, is_resursive))
-
-        self._event_tick = threading.Event()
         self._event_kill = threading.Event()
+        self._thread_tick = threading.Thread(
+            target=self._tick,
+            args=(target, interval))
 
     def _tick(self, *args, **kwargs):
-        interval_sec = args[0]
-        is_recursive = args[1]
+        target = args[0]
+        interval = args[1]
 
-        if is_recursive:
-            thread_main = threading.Thread(target=self._main)
-            thread_main.start()
+        event_tick = threading.Event()
+        thread_target = threading.Thread(
+            target=self._recurs_func,
+            args=(target, event_tick))
 
-            start_time = datetime.datetime.now
+        thread_target.start()
 
-            while not self._event_kill.isSet():
-                time.sleep(1)
-                now_time = datetime.datetime.now
+        start_time = datetime.datetime.now()
 
-                if (now_time - start_time).seconds > interval_sec:
-                    self._event_tick.set()
-                    start_time = now_time
-
-            thread_main.join()
-        else:
-            time.sleep(interval_sec)
-            self._func()
-
-    def _main(self):
         while not self._event_kill.isSet():
-            self._event_tick.wait()
-            self._func()
-            self._event_tick.clear()
+            time.sleep(1)
+            now_time = datetime.datetime.now()
+
+            if (now_time - start_time).seconds >= interval:
+                event_tick.set()
+                start_time = now_time
+
+        event_tick.set()
+        thread_target.join()
+
+    def _recurs_func(self, *args, **kwargs):
+        target = args[0]
+        event_tick = args[1]
+
+        while not self._event_kill.isSet():
+            event_tick.wait()
+            target()
+            event_tick.clear()
 
     def start(self):
         if not self._thread_tick.isAlive():
             self._event_kill.clear()
-            self._event_tick.clear()
             self._thread_tick.start()
         else:
             raise SystemError("timer thread is already run")
