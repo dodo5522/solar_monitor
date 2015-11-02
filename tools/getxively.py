be81ccfaeb13895d9bc7d4bbf9a27b7adba338e9
Takashi Ando <dodo5522@gmail.com>
2015-11-02 22:08:27 +0900

    Add some functions.

--- tools/getxively.py
+++ tools/getxively.py
@@ -6,6 +6,7 @@
 import sys
 import argparse
 import requests
+import json
 import datetime
 import time
 
@@ -13,7 +14,6 @@ DEFAULT_INTERVAL_SEC = 600
 DEFAULT_INTERVAL_TYPE = 'discrete'
 DEFAULT_DURATION_SEC = 3600
 DEFAULT_START_DATE = "2015-10-01T00:00:00Z"
-DEFAULT_FORMAT = 'json'
 DEFAULT_FEED = '274175384'
 URL = "https://api.xively.com/v2/feeds/{}/datastreams"
 
@@ -51,8 +51,7 @@ def init_args(args=sys.argv[1:]):
             "-f", "--feed",
             type=str,
             default=DEFAULT_FEED,
-            help="Data format getting from xively." +
-                 "Default is " + DEFAULT_FORMAT + ".")
+            help="Feed ID for xively.")
 
     parser.add_argument(
             "-i", "--interval",
@@ -78,29 +77,54 @@ def init_args(args=sys.argv[1:]):
     return parser.parse_args(args)
 
 
+def get_start_end(start_date_str, duration):
+    start = datetime.datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%SZ')
+    end = start + datetime.timedelta(0, duration)
+    return (start, end)
+
+
+def get_stream_data(user, password, feed, stream, interval, start, end):
+    url = '/'.join((URL.format(feed), stream + '.json?'))
+    url = '&'.join((url,
+                    'interval=' + str(interval),
+                    'interval_type=' + DEFAULT_INTERVAL_TYPE,
+                    'start=' + start.isoformat() + 'Z',
+                    'end=' + end.isoformat() + 'Z'))
+    return requests.get(url, auth=(user, password)).json()
+
+
 if __name__ == "__main__":
     args = init_args()
 
     interval = args.interval
     duration = args.duration
-    start = args.start_date
-    start = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%SZ')
-    end = start + datetime.timedelta(0, duration)
 
+    if duration / interval > 1000:
+        raise ValueError("Xively has a limit of maximum data number 1000.")
+
+    start, end = get_start_end(args.start_date, duration)
     user = args.user
     password = args.password
     feed = args.feed
 
-    for n, stream in enumerate(STREAMS):
-        url = '/'.join((URL.format(feed), stream + '.' + DEFAULT_FORMAT + '?'))
-        url = '&'.join((url, 'interval=' + str(interval),
-                        'interval_type=' + DEFAULT_INTERVAL_TYPE,
-                        'start=' + start.isoformat() + 'Z',
-                        'end=' + end.isoformat() + 'Z'))
-        data = requests.get(url, auth=(user, password))
+    for stream in STREAMS:
+        time_out = 10
 
-        with open('xively_' + stream + '.' + DEFAULT_FORMAT, "w") as fp:
-            fp.write(data.text)
+        while time_out:
+            data = get_stream_data(
+                    user, password, feed, stream, interval, start, end)
 
-        # must wait for 10 seconds minimum on xively to get next stream data.
-        time.sleep(11)
+            if "errors" not in data:
+                break
+
+            print("{}: {}, {}".format(time_out, data["title"], data["errors"]))
+
+            # must wait for 10 seconds min on xively to get next stream data.
+            time.sleep(10)
+
+            time_out -= 1
+        else:
+            continue
+
+        with open('xively_' + stream + '.json', "w") as fp:
+            fp.write(json.dumps(data))
