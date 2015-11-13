1ed3dbf3ca98917a636d0b606ce6d5101cd97108
Takashi Ando <dodo5522@gmail.com>
2015-11-14 08:07:15 +0900

    Correct package structure.

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ from setuptools import setup
 
 setup(
     name='tsmppt60_monitor',
-    version='1.2.0',
+    version='1.3.0',
     description='TS-MPPT-60 charge controller monitor',
     author='Takashi Ando',
     url='https://github.com/dodo5522/tsmppt60_monitor',
     packages=[
-        "hook"],
-    py_modules=[
         "tsmppt60_monitor",
-        "timer"],
+        "tsmppt60_monitor.hook"],
     install_requires=[
         "requests>=2.6.0",
         "xively-python>=0.1.0-rc1",
