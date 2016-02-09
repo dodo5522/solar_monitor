f6ae5713f34fd08137505878a3fe9c76bd9fd667
Takashi Ando <dodo5522@gmail.com>
2016-02-10 08:21:24 +0900

    Modify timer.py with more readable.

--- solar_monitor/timer.py
+++ solar_monitor/timer.py
@@ -12,39 +12,53 @@ import threading
 from tsmppt60_driver.base import Logger
 
 
+class AlreadyRunningError(Exception):
+    pass
+
+
 class RecursiveTimer(Logger):
-    """ Class of timer for recursively running function. """
-    def __init__(self, target, interval=300, **kwargs):
-        """ Recursive timer
+    """Class of timer for recursively running function.
+    """
 
-        Arguments:
-            target: callable object to run by timer event.
+    def __init__(self, target_func, interval=300, log_file_path=None, debug=False):
+        """Recursive timer
+
+        Keyword arguments:
+            target_func: callable object to run by timer event.
             interval: interval time as second
+            log_file_path: file path of log file to output
+            debug: enable debug mode if True
+
+        Returns:
+            timer object
         """
-        Logger.__init__(
-            self,
-            log_file_path=kwargs.get("log_file_path"),
-            debug=kwargs.get("debug"))
+        Logger.__init__(self, log_file_path, debug)
 
-        self._event_kill = threading.Event()
-        self._thread_tick = threading.Thread(
+        self.event_stop_timer = threading.Event()
+        self.thread_timer = threading.Thread(
             target=self._tick,
-            args=(target, interval))
+            args=(target_func, interval))
 
-    def _tick(self, *args, **kwargs):
-        target = args[0]
-        interval = args[1]
+    def _tick(self, target_func, interval, **kwargs):
+        """Tick the time with the specified interval and call the target function.
 
+        Keyword arguments:
+            target_func: target function to be called recursively
+            interval: interval time second
+
+        Returns:
+            None
+        """
         event_tick = threading.Event()
-        thread_target = threading.Thread(
-            target=self._recurs_func,
-            args=(target, event_tick))
+        thread_mainloop = threading.Thread(
+            target=self._main_loop,
+            args=(target_func, event_tick))
 
-        thread_target.start()
+        thread_mainloop.start()
 
         start_time = datetime.datetime.now()
 
-        while not self._event_kill.isSet():
+        while not self.event_stop_timer.isSet():
             time.sleep(1)
             now_time = datetime.datetime.now()
 
@@ -53,30 +67,35 @@ class RecursiveTimer(Logger):
                 start_time = now_time
 
         event_tick.set()
-        thread_target.join()
+        thread_mainloop.join()
 
-    def _recurs_func(self, *args, **kwargs):
-        target = args[0]
-        event_tick = args[1]
+    def _main_loop(self, target_func, event_tick, **kwargs):
+        """Loop with the specified function.
 
-        while not self._event_kill.isSet():
+        Keyword arguments:
+            target_func: target function to be called recursively
+            event_tick: event to be set at the timing of calling target_func
+
+        Returns:
+            None
+        """
+        while not self.event_stop_timer.isSet():
             event_tick.wait()
 
             try:
-                target()
-            except Exception as err:
+                target_func()
+            except Exception as e:
                 self.logger.debug(str(e) + ' error!!!')
-                raise
 
             event_tick.clear()
 
     def start(self):
-        if not self._thread_tick.isAlive():
-            self._event_kill.clear()
-            self._thread_tick.start()
+        if not self.thread_timer.isAlive():
+            self.event_stop_timer.clear()
+            self.thread_timer.start()
         else:
-            raise SystemError("timer thread is already run")
+            raise AlreadyRunningError("timer thread is already run")
 
     def cancel(self):
-        self._event_kill.set()
-        self._thread_tick.join()
+        self.event_stop_timer.set()
+        self.thread_timer.join()
