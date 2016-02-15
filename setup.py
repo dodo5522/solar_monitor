36846b8741d085a7c282ecacbc55164d7a385bec
Takashi Ando <dodo5522@gmail.com>
2016-02-16 06:47:32 +0900

    v1.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def requires():
 
 setup(
     name='solar_monitor',
-    version='1.4.1',
+    version='1.5.0',
     description='Solar charge controller monitor like TS-MPPT-60',
     long_description=readme(),
     license="GPLv2",
