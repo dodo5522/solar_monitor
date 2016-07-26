f1bd1c95e650dac2567be43e0d2c33697dab3494
Takashi Ando <dodo5522@gmail.com>
2016-07-26 23:43:56 +0900

    Add test case for put_q() of multiple event handlers in an event trigger.

--- test/test_event_trigger.py
+++ test/test_event_trigger.py
@@ -3,7 +3,7 @@
 
 import unittest
 from solar_monitor.event.base import IEventTrigger
-from solar_monitor.event.base import IEventHandler
+from solar_monitor.event.base import IEventListener
 from unittest.mock import MagicMock
 
 
@@ -32,22 +32,29 @@ class TestEventTrigger(unittest.TestCase):
         et = IEventTrigger(q_max=10)
         self.assertEqual(et.q_.maxsize, 10)
 
-    def test_start_stop_event_handlers(self):
-        """ 複数EventHandlerを登録し、startコールで全event handlerが開始されること、stopコールで全event handlerが停止されることをテストする """
-        handler1 = MagicMock(spec=IEventHandler)
-        handler2 = MagicMock(spec=IEventHandler)
+    def test_start_put_stop_event_handlers(self):
+        """ 複数EventHandlerを登録し、startコールで全event handlerが開始されること、put_qコールで全event handlerにデータ送信されること、stopコールで全event handlerが停止されることをテストする """
+        handler1 = MagicMock(spec=IEventListener)
+        handler2 = MagicMock(spec=IEventListener)
 
-        et = IEventTrigger()
+        class TestTrigger(IEventTrigger):
+            def _is_condition(self, data):
+                return True
+
+        et = TestTrigger()
         et.append(handler1)
         et.append(handler2)
 
         et.start()
+        et.put_q(1)
+        et.stop()
+        et.join()
 
         self.assertEqual(handler1.start.call_count, 1)
         self.assertEqual(handler2.start.call_count, 1)
 
-        et.stop()
-        et.join()
+        self.assertEqual(handler1.put_q.call_count, 1)
+        self.assertEqual(handler2.put_q.call_count, 1)
 
         self.assertEqual(handler1.stop.call_count, 1)
         self.assertEqual(handler1.join.call_count, 1)
