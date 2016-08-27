65de6c24811970dfd5076b61f74c80c62dd8ce5f
Takashi Ando <dodo5522@gmail.com>
2016-08-27 11:09:27 +0900

    config.pyテスト雛形を追加 #21

---
+++ test/test_config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#   Copyright 2016 Takashi Ando - http://blog.rinka-blossom.com/
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import unittest
+from solar_monitor import argparser
+from solar_monitor import config
+
+
+class TestConfig(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_default_config(self):
+        args = argparser.init([])
+        kwargs = dict(args._get_kwargs())
+
+        triggers = config.init_triggers(**kwargs)
+
+
+if __name__ == "__main__":
+    unittest.main()
