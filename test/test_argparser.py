2fd35246d780c079ec390789fb4199a22a25a631
Takashi Ando <dodo5522@gmail.com>
2016-11-06 01:11:34 +0900

    argparseのテスト内容を修正

--- test/test_argparser.py
+++ test/test_argparser.py
@@ -35,7 +35,10 @@ class TestArgParser(unittest.TestCase):
         pass
 
     def test_default_args(self):
-        parsed = argparser.init([])
+        self.assertRaises(SystemExit, argparser.init, [])
+
+    def test_optional_args(self):
+        parsed = argparser.init(["192.168.1.20"])
 
         self.assertEqual("192.168.1.20", parsed.host_name)
         self.assertEqual(None, parsed.xively_api_key)
